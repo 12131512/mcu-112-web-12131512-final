2ec716288da301f048a8603a5372ede944f1848d
12131512 <12131512@me.mcu.edu.tw>
2024-01-11 15:50:27 +0800

    feat(title): 標題列元件建立

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavBarComponent,
+    HeaderComponent,
+    HomePageComponent,
+  ],
 })
 export class AppComponent {
-  title = 'mcu-112-web-12131512-final';
+  title = 'mcu-112-web-09130456-final';
 }
