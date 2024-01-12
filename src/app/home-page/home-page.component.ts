ee2abf245e254a9e4f6af49625e6e69fc8ff01bc
12131512 <12131512@me.mcu.edu.tw>
2024-01-12 21:31:22 +0800

    feat(body):首頁內容元件實作新增

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { BodyComponent } from '../body/body.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
+  imports: [HeaderComponent, BodyComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
-  imports: [CommonModule, HeaderComponent],
 })
 export class HomePageComponent {}
