ea307564bc9980e931f94227c69f30a352b64224
12131512 <12131512@me.mcu.edu.tw>
2024-01-15 21:14:52 +0800

    refactor(tad-sidebar):標籤側欄元件實作新增

---
+++ src/app/tag-sidebar/tag-sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tag-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tag-sidebar.component.html',
+  styleUrl: './tag-sidebar.component.css'
+})
+export class TagSidebarComponent {
+
+}
